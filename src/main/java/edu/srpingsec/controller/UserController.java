111f498b0913a5d8780b236488f9243ac09ff4eb
Rashen <rashen598@gmail.com>
2023-12-15 15:27:55 +0530

    added a post mapping to register a user

--- src/main/java/edu/srpingsec/controller/UserController.java
+++ src/main/java/edu/srpingsec/controller/UserController.java
@@ -1,11 +1,13 @@
 package edu.srpingsec.controller;
 
+import edu.srpingsec.dao.UserEntity;
+import edu.srpingsec.dto.User;
+import edu.srpingsec.repository.UserRepository;
 import edu.srpingsec.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/log")
     public String loginMethod(){
@@ -22,6 +27,25 @@ public class UserController {
     @GetMapping("/register")
     public String singUpMethod(){
         return "Registration Success!";
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody UserEntity user) {
+        UserEntity savedUser = null;
+        ResponseEntity response = null;
+        try {
+            savedUser = userRepository.save(user);
+            if (savedUser.getUserId() > 0) {
+                response = ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body("Given user details are successfully registered");
+            }
+        } catch (Exception ex) {
+            response = ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception occured due to " + ex.getMessage());
+        }
+        return response;
     }
 
 }
